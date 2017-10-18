54489705fcacf7d688a7b3a3f77c728ff5294c93
Stureplan <linus.ax@gmail.com>
2017-10-18 16:19:35 +0200

    Comment added

--- TA_ParticleEditor/TA_ParticleEditor/Program.cs
+++ TA_ParticleEditor/TA_ParticleEditor/Program.cs
@@ -11,6 +11,7 @@ namespace TA_ParticleEditor
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        // comment
         [STAThread]
         static void Main()
         {
